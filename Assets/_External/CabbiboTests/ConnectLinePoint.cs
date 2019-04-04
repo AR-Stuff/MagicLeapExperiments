b738972a795363cf9a90a12e0b10885d6a79829c
Eric Provencher <eprovencher92@gmail.com>
2019-04-04 17:54:38 -0400

    Imported Cabbibo tests

---
+++ Assets/_External/CabbiboTests/ConnectLinePoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectLinePoint : MonoBehaviour {
+  public Transform o;
+  private LineRenderer lr;
+	// Use this for initialization
+	void Start () {
+    lr = GetComponent<LineRenderer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( o != null ){
+      lr.SetPosition(0 , transform.position);
+      lr.SetPosition(1 , o.position);
+    }else{
+      lr.SetPosition(0 , transform.position);
+    lr.SetPosition(1 , transform.position);
+    }
+	}
+}
---
+++ Assets/_External/CabbiboTests/SwapChildrenMaterials.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapChildrenMaterials : MonoBehaviour {
+
+  public Material[] materials;
+  public PhysicMaterial physicsMat;
+  public int currentMaterial = 0;
+
+	// Use this for initialization
+	void Start () {
+		SetPhysicsMaterials();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetMaterials();
+	}
+
+  public void Up(){
+    currentMaterial += 1;
+    currentMaterial %= materials.Length;
+    SetMaterials();
+  }
+
+  public void Down(){
+    currentMaterial -= 1;
+    currentMaterial %= materials.Length;
+    SetMaterials();
+  }
+
+  void SetMaterials(){
+    Renderer[] renderers = GetComponentsInChildren<Renderer>();
+    foreach (var r in renderers)
+    {
+        r.gameObject.tag = "Mesh";
+       r.material = materials[currentMaterial];
+    }
+
+  }
+
+   void SetPhysicsMaterials(){
+    Collider[] renderers = GetComponentsInChildren<Collider>();
+    foreach (var r in renderers)
+    {
+       r.material = physicsMat;
+    }
+
+  }
+}
